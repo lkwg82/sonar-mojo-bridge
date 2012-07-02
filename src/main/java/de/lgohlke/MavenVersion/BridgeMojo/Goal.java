d75b97609cff399e852c4d0edcf712ae2f7a65ff
Lars Gohlke <lkwg82@gmx.de>
2012-07-02 02:27:04 +0200

    "[PATCH] added annotation to show goal"

---
+++ src/main/java/de/lgohlke/MavenVersion/BridgeMojo/Goal.java
@@ -0,0 +1,16 @@
+package de.lgohlke.MavenVersion.BridgeMojo;
+
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Retention(RUNTIME)
+@Target(TYPE)
+@Inherited
+public @interface Goal {
+  String value();
+
+}
