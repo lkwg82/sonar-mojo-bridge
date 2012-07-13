267bb59b9dfb61aa25db8e14b93b85366579181b
Lars Gohlke <lars.gohlke@immobilienscout24.de>
2012-07-13 14:04:52 +0200

    added scope for enum fields

--- src/main/java/de/lgohlke/sonar/maven/Goal.java
+++ src/main/java/de/lgohlke/sonar/maven/Goal.java
@@ -22,13 +22,14 @@ package de.lgohlke.sonar.maven;
 import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
-
+import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@Retention(RUNTIME)
-@Target(TYPE)
+
 @Inherited
+@Retention(RUNTIME)
+@Target({ TYPE, FIELD })
 public @interface Goal {
   String value();
 
