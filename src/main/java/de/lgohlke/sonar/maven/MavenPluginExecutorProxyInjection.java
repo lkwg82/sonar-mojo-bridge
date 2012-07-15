c22f45e048befe78b597836938eb6ad65790d17f
Lars Gohlke <lkwg82@gmx.de>
2012-07-16 00:42:15 +0200

    extracted some inner classes

--- src/main/java/de/lgohlke/sonar/maven/MavenPluginExecutorProxyInjection.java
+++ src/main/java/de/lgohlke/sonar/maven/MavenPluginExecutorProxyInjection.java
@@ -19,6 +19,7 @@
  */
 package de.lgohlke.sonar.maven;
 
+import de.lgohlke.sonar.maven.plugin.versions.DisplayDependencyMojoProxy;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.lifecycle.LifecycleExecutor;
 import org.apache.maven.lifecycle.internal.MojoExecutor;
@@ -26,10 +27,10 @@ import org.apache.maven.plugin.BuildPluginManager;
 import org.apache.maven.plugin.MavenPluginManager;
 import org.apache.maven.plugin.Mojo;
 import org.codehaus.plexus.PlexusContainer;
+import org.codehaus.plexus.classworlds.realm.ClassRealm;
 import org.sonar.batch.MavenPluginExecutor;
 import org.sonar.maven3.Maven3PluginExecutor;
 
-import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
@@ -45,29 +46,16 @@ public class MavenPluginExecutorProxyInjection {
 
   private static void decorateMaven3ExecutionProcess(final MavenPluginExecutor mavenPluginExecutor) {
     try {
-      LifecycleExecutor lifecycleExecutor = field("lifecycleExecutor").
-          ofType(LifecycleExecutor.class).
-          in(mavenPluginExecutor).get();
-
-      MavenSession mavenSession = field("mavenSession").
-          ofType(MavenSession.class).
-          in(mavenPluginExecutor).get();
-
+      LifecycleExecutor lifecycleExecutor = field("lifecycleExecutor").ofType(LifecycleExecutor.class).in(mavenPluginExecutor).get();
+      MavenSession mavenSession = field("mavenSession").ofType(MavenSession.class).in(mavenPluginExecutor).get();
       MojoExecutor mojoExecutor = field("mojoExecutor").ofType(MojoExecutor.class).in(lifecycleExecutor).get();
       BuildPluginManager pluginManager = field("pluginManager").ofType(BuildPluginManager.class).in(mojoExecutor).get();
       PlexusContainer container = field("container").ofType(PlexusContainer.class).in(mavenSession).get();
-
       MavenPluginManager mavenPluginManager = container.lookup(MavenPluginManager.class);
       mavenPluginManager = getMavenPluginManagerProxy(MavenPluginManager.class, mavenPluginManager);
-      // container.release(mavenPluginManager);
-      // container.addComponent(mavenPluginManager, MavenPluginManager.class.getName());
-      // mavenPluginManager = container.lookup(MavenPluginManager.class);
-      // System.out.println(pluginManager);
       field("mavenPluginManager").ofType(MavenPluginManager.class).in(pluginManager).set(mavenPluginManager);
-      // System.out.println(mavenPluginManager);
     } catch (Exception e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
+      throw new IllegalStateException(e);
     }
   }
 
@@ -79,7 +67,7 @@ public class MavenPluginExecutorProxyInjection {
 
 }
 
-class MavenPluginManagerProxy<T extends MavenPluginManager> extends ProxyAbstract<T> {
+class MavenPluginManagerProxy<T extends MavenPluginManager> extends DynamicProxy<T> {
 
   public MavenPluginManagerProxy(final T underlying) {
     super(underlying);
@@ -87,88 +75,24 @@ class MavenPluginManagerProxy<T extends MavenPluginManager> extends ProxyAbstrac
 
   @Override
   public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
-
     Object result = method.invoke(getUnderLying(), args);
     if (method.getName().equals("getConfiguredMojo")) {
-      final HelpMojoProxy<Mojo> proxymojo = new HelpMojoProxy<Mojo>((Mojo) result);
+
+      final ClassLoader classLoader = result.getClass().getClassLoader();
+      {
+        ClassRealm x = (ClassRealm) classLoader;
+        x.importFrom(getClass().getClassLoader(), "de.lgohlke");
+        System.out.println(x + "");
+      }
+
+      Class<?> clazz = classLoader.loadClass(DisplayDependencyMojoProxy.class.getCanonicalName());
+      // final HelpMojoProxy<Mojo> proxymojo = new HelpMojoProxy<Mojo>();
+      DisplayDependencyMojoProxy<Mojo> proxymojo = (DisplayDependencyMojoProxy<Mojo>) clazz.newInstance();
+      // new DisplayDependencyMojoProxy<Mojo>();
+      proxymojo.setUnderlying((Mojo) result);
       result = Proxy.newProxyInstance(result.getClass().getClassLoader(), new Class<?>[] {Mojo.class}, proxymojo);
     }
 
     return result;
   }
-}
-
-class HelpMojoProxy<T extends Mojo> extends MyMojoProxy<T> {
-
-  public HelpMojoProxy(final T underlying) {
-    super(underlying);
-  }
-
-  @Override
-  public void beforeExecution(final Mojo mojo) {
-    System.out.println("before " + mojo);
-  }
-
-  @Override
-  public void afterExecution(final Mojo mojo) {
-    System.out.println("after " + mojo);
-  }
-
-  @Override
-  public boolean useAlternativeExecute() {
-    return true;
-  }
-
-}
-
-abstract class ProxyAbstract<T> implements InvocationHandler {
-  private T underlying;
-
-  public ProxyAbstract(final T underlying) {
-    this.underlying = underlying;
-  }
-
-  public T getUnderLying() {
-    return underlying;
-  }
-
-  public void setUnderlying(final T underlying) {
-    this.underlying = underlying;
-  }
-}
-
-abstract class MyMojoProxy<T extends Mojo> extends ProxyAbstract<T> {
-
-  public MyMojoProxy(final T underlying) {
-    super(underlying);
-  }
-
-  public abstract void beforeExecution(T mojo);
-
-  public abstract void afterExecution(T mojo);
-
-  public abstract boolean useAlternativeExecute();
-
-  public void alternativeExecute(final T mojo) {
-    // can be overriden
-  }
-
-  @Override
-  public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
-
-    Object result = null;
-    if (method.getName().equals("execute") && args == null) {
-      beforeExecution(getUnderLying());
-      if (useAlternativeExecute()) {
-        alternativeExecute(getUnderLying());
-      } else {
-        result = method.invoke(getUnderLying(), args);
-      }
-      afterExecution(getUnderLying());
-    } else {
-      result = method.invoke(getUnderLying(), args);
-    }
-    return result;
-  }
-
 }
