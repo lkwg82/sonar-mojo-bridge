4f9af635f6464859fc3ff381a7ecf26c08e67c73
Lars Gohlke <lkwg82@gmx.de>
2013-03-19 23:45:35 +0100

    cleanup

--- src/main/java/de/lgohlke/sonar/MavenPlugin.java
+++ src/main/java/de/lgohlke/sonar/MavenPlugin.java
@@ -31,19 +31,24 @@ import org.sonar.plugins.xml.language.Xml;
 import java.util.Arrays;
 import java.util.List;
 
-
 @Properties(
     {
         @Property(
-            key = MavenPlugin.ANALYSIS_ENABLED, name = "enable maven analysis", description = "Enable maven analysis.",
-            defaultValue = MavenPlugin.DEFAULT, global = true, project = true, type = PropertyType.BOOLEAN
+            key = MavenPlugin.ANALYSIS_ENABLED,
+            name = "enable maven analysis",
+            description = "Enable maven analysis.",
+            defaultValue = MavenPlugin.DEFAULT,
+            global = true,
+            project = true,
+            type = PropertyType.BOOLEAN
         )
     }
 )
 public class MavenPlugin extends SonarPlugin {
-  public static final String ANALYSIS_ENABLED = "sonar.maven.analysis";
+  public static final String PLUGIN_KEY      = "sonar.maven";
+  public static final String ANALYSIS_ENABLED = PLUGIN_KEY +".analysis";
   public static final String REPOSITORY_KEY = "maven";
-  public static final String REPOSITORY_NAME = "Maven";
+  static final String REPOSITORY_NAME = "Maven";
   public static final String DEFAULT = "true";
 
   @Override
