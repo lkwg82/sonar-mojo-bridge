a067977e921cfed1f7dddd12d7ad74b541e10683
lkwg82 <lkwg82@gmx.de>
2013-05-11 18:42:19 +0200

    splitted into separate modules

---
+++ modules/sonar-plugin/src/test/java/de/lgohlke/sonar/MavenPluginTest.java
@@ -0,0 +1,56 @@
+/*
+ * Sonar maven checks plugin
+ * Copyright (C) 2012 Lars Gohlke
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package de.lgohlke.sonar;
+
+import org.fest.assertions.core.Condition;
+import org.sonar.api.batch.Sensor;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+/**
+ * User: lgohlke
+ */
+public class MavenPluginTest {
+  @Test
+  public void shouldHaveImportantExtensions() throws Exception {
+    MavenPlugin plugin = new MavenPlugin();
+
+    List<Class<?>> extensions = plugin.getExtensions();
+    assertThat(extensions).has(classOfSubType(Sensor.class));
+    assertThat(extensions).has(classOfSubType(RulesRepository.class));
+  }
+
+  private Condition<List<Class<?>>> classOfSubType(final Class<?> clazz) {
+    return new Condition<List<Class<?>>>() {
+      @Override
+      public boolean matches(List<Class<?>> list) {
+        for (Class<?> c : list) {
+          if (clazz.isAssignableFrom(c)) {
+            return true;
+          }
+        }
+        return false;
+      }
+    };
+  }
+}
