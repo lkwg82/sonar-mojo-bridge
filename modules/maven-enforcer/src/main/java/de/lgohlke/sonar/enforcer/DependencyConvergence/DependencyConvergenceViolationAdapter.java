77ffe7d2e3ef23e0af2cc36d856695bfc19464e3
lkwg82 <lkwg82@gmx.de>
2013-05-11 18:41:36 +0200

    splitted into separate modules

---
+++ modules/maven-enforcer/src/main/java/de/lgohlke/sonar/enforcer/DependencyConvergence/DependencyConvergenceViolationAdapter.java
@@ -0,0 +1,75 @@
+/*
+ * sonar-maven-checks-maven-enforcer
+ * Copyright (C) 2012 Lars Gohlke
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package de.lgohlke.sonar.enforcer.DependencyConvergence;
+
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
+import de.lgohlke.sonar.enforcer.ViolationAdapter;
+import lombok.Setter;
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.shared.dependency.tree.DependencyNode;
+import org.sonar.api.resources.File;
+import org.sonar.api.rules.Rule;
+import org.sonar.api.rules.Violation;
+import org.sonar.plugins.xml.language.Xml;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * User: lgohlke
+ */
+public class DependencyConvergenceViolationAdapter extends ViolationAdapter<DependencyConvergenceRule> {
+  @Setter
+  private List<List<DependencyNode>> errors;
+
+  public DependencyConvergenceViolationAdapter(MavenProject mavenProject) {
+    super(mavenProject);
+  }
+
+  @Override
+  public List<Violation> getViolations() {
+    List<Violation> violations = new ArrayList<Violation>();
+
+    File file = new File("", getMavenProject().getFile().getName());
+    file.setLanguage(Xml.INSTANCE);
+
+    Rule rule = getRule(DependencyConvergenceRule.class);
+
+    for (List<DependencyNode> error : errors) {
+      Violation violation = Violation.create(rule, file);
+      violation.setLineId(1);
+      violation.setMessage(createMessage(error));
+      violations.add(violation);
+    }
+    return violations;
+  }
+
+  private String createMessage(List<DependencyNode> nodeList) {
+    final DependencyNode dependencyNode = nodeList.get(0);
+    final Artifact artifact = dependencyNode.getArtifact();
+    List<String> versions = Lists.newArrayList();
+    for (DependencyNode node : nodeList) {
+      versions.add(node.getArtifact().getVersion());
+    }
+    return "found multiple version for " + artifact.getGroupId() + ":" + artifact.getArtifactId() + " (" + Joiner.on(",").join(versions) + ")";
+  }
+}
---
+++ modules/maven-enforcer/src/main/java/de/lgohlke/sonar/enforcer/ViolationAdapter.java
@@ -0,0 +1,47 @@
+/*
+ * sonar-maven-checks-maven-enforcer
+ * Copyright (C) 2012 Lars Gohlke
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package de.lgohlke.sonar.enforcer;
+
+import de.lgohlke.sonar.maven.MavenRule;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.apache.maven.project.MavenProject;
+import org.sonar.api.rules.Rule;
+import org.sonar.api.rules.Violation;
+
+import java.util.List;
+
+/**
+ * User: lgohlke
+ */
+@RequiredArgsConstructor
+public abstract class ViolationAdapter<T extends MavenRule> {
+  @Getter(AccessLevel.PROTECTED)
+  private final MavenProject mavenProject;
+
+  public abstract List<Violation> getViolations();
+
+  // TODO allgemein machen
+  protected Rule getRule(Class<T> ruleClass) {
+    String key = ruleClass.getAnnotation(org.sonar.check.Rule.class).key();
+    return Rule.create(de.lgohlke.sonar.Configuration.REPOSITORY_KEY, key);
+  }
+}
