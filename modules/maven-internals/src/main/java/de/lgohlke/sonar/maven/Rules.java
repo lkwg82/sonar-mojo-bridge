c78d96bff760432dbf337c9f820f3eff8b49a359
lkwg82 <lkwg82@gmx.de>
2013-08-25 20:42:55 +0200

    Rules annotation now inherited

--- modules/maven-internals/src/main/java/de/lgohlke/sonar/maven/Rules.java
+++ modules/maven-internals/src/main/java/de/lgohlke/sonar/maven/Rules.java
@@ -19,17 +19,11 @@
  */
 package de.lgohlke.sonar.maven;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
-
-/**
- * User: lars
- */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
+@Inherited
 public @interface Rules {
   Class<? extends MavenRule>[] values() default {};
 }
