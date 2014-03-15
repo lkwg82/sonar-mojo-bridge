87eb2f8ddbb6c88a682942a00e75da8507f480dc
Lars K.W. Gohlke <lkwg82@gmx.de>
2014-03-15 17:23:20 +0100

    added deploy script for local sonarqube instance

---
+++ deploy_local.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+sonarVersion=sonar-3.7.4
+sonarDir=/home/lars/development/tools/$sonarVersion
+sonarBin=$sonarDir/bin/linux-x86-64/sonar.sh
+pluginDir=$sonarDir/extensions/plugins/
+baseName=sonar-mojo-bridge-*.jar
+
+rm -v $pluginDir/$baseName
+mv -v ./modules/sonar-plugin/target/$baseName $pluginDir
+
+$sonarBin restart &
+tail -f $sonarDir/logs/*log
