65de45a2bfd150a5b56b915255e4ffc8298d0fa2
Lars K.W. Gohlke <lkwg82@gmx.de>
2014-03-18 02:21:20 +0100

    removed unused method

---
+++ modules/maven-enforcer/src/test/java/de/lgohlke/sonar/maven/versions/TestHelper.java
@@ -0,0 +1,39 @@
+/*
+ * sonar-mojo-bridge-maven-enforcer
+ * Copyright (C) 2012 Lars Gohlke
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package de.lgohlke.sonar.maven.versions;
+
+import org.apache.maven.project.MavenProject;
+
+import java.io.File;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+/**
+ * User: lars
+ */
+public class TestHelper {
+    static MavenProject getMavenProject() {
+        MavenProject mavenProject = mock(MavenProject.class);
+        when(mavenProject.getFile()).thenReturn(new File("."));
+        return mavenProject;
+    }
+}
